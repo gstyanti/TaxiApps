fa001145b0f95b4c9a85282259104f0e4089f301
gstyanti <101311934+gstyanti@users.noreply.github.com>
2022-03-22 22:52:16 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxiapps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat objek dari class Taxi
+            Taxi driver = new Taxi();
+            Taxi penumpang = new Taxi();
+
+            //mengeset nilai properties objek driver dan penumpang
+            driver.Driver_Name = "Agus";
+            driver.jumlah = "3";
+            driver.On_Duty = "sedang bertugas";
+
+            penumpang.Driver_Name = "Ari";
+            penumpang.jumlah = "2";
+            penumpang.On_Duty = "sedang dalam perjalanan";
+
+            //memanggil method Taxiinfo, Jemput Penumpang, dan penurunan penumpang
+            driver.Taxi_info();
+            driver.Pick_Up_Passenger();
+            driver.Drop_Of_Passenger();
+
+            penumpang.Taxi_info();
+            penumpang.Pick_Up_Passenger();
+            penumpang.Drop_Of_Passenger(); 
+
+            Console.ReadKey();
+
+    
+
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxiapps
+{
+   public class Taxi
+    {
+        //properties 
+        public string Driver_Name { get; set; }
+        public string On_Duty { get; set; }
+        public string Num_Passenger { get; set; }
+        public string jumlah { get; set; }
+
+        //Method
+        public void Taxi_info()
+        {
+            Console.WriteLine("Nama Driver: {0}", Driver_Name);
+            Console.WriteLine("sedang bertugas: {0}", On_Duty);
+            Console.WriteLine("Jumlah Penumpang: {0}", jumlah );
+            Console.WriteLine("Num_Passenger: {0}", Num_Passenger);
+            Console.WriteLine("Anda Telah Melakukan Pemesanan Taxi");
+        }
+
+        public void Pick_Up_Passenger()
+        {
+            Console.WriteLine("{0} Driver sedang menuju ke lokasi anda, silahkan menunggu\n", Driver_Name);
+        }
+
+        public void Drop_Of_Passenger()
+        {
+            Console.WriteLine("{0} Lokasi tujuan anda sudah dekat ", Num_Passenger);
+        }
+    }
+}
